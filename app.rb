5127f9ec2f32b38393f18f5f0bee13963b89135e
Shawn Xu <shallway.xu@gmail.com>
2012-10-09 00:00:28 +0800

    slug & utf-8 output

--- app.rb
+++ app.rb
@@ -1,7 +1,38 @@
+# -*- coding: utf-8 -*-
 require 'sinatra'
 require 'json'
 require 'yaml'
 require './models/page.rb'
+
+require 'unidecode'
+# put in extensions.rb
+String.class_eval do
+  def to_slug
+    self.transliterate.downcase.gsub(/[^a-z0-9 ]/, ' ').strip.gsub(/[ ]+/, '-')
+  end
+
+  # differs from the 'to_slug' method in that it leaves in the dot '.'
+  # character and removes Windows' crust from paths (removes
+  # "C:\Temp\" from "C:\Temp\mieczyslaw.jpg")
+  def sanitize_as_filename
+    self.gsub(/^.*(\\|\/)/, '').transliterate.downcase.gsub(/[^a-z0-9\. ]/, ' ').strip.gsub(/[ ]+/, '-')
+  end
+
+  def transliterate
+    # Unidecode gem is missing some hyphen transliterations
+    self.gsub(/[-‐‒–—―⁃−­]/, '-').to_ascii
+  end
+end
+
+
+def return_html
+  content_type 'text/html', :charset => 'utf-8'
+end
+def return_json
+  content_type 'application/json', :charset => 'utf-8'
+end
+
+
 
 #class App < SInatra::Base
 
@@ -10,21 +41,11 @@ enable :sessions # not the best way
 set :public_folder, File.dirname(__FILE__) + '/static'
 
 before do
-  headers "Content-Type" => "application/json"
-end
-
-get '/test' do
-  bbcode    = ":bbcode\n[b]bold text[/b]"
-  markdown  = ":markdown\n**bold text**"
-  mediawiki = ":mediawiki\n'''bold text'''"
-  orgmode   = ":orgmode\n*bold text*"
-  textile   = ":textile\n*bold text*"
-  raw       = "*bold text*<style>\nh1 {color:red}</style><style>h2 {color:blue}</style>"
-  Text.new(raw).text
+  return_json
 end
 
 get '/user/signup' do
-  headers "Content-Type" => "text/html"
+  return_html
   erb :signup
 end
 
@@ -37,7 +58,7 @@ end
 
 get '/user/signin' do
   # return 'logged in already' if session[:uid]
-  headers "Content-Type" => "text/html"  
+  return_html
   erb :signin
 end
 
@@ -48,23 +69,6 @@ post '/user/signin' do
 end
 
 get '/user/signout' do
-end
-
-get '/page/:slug' do
-  page   = Page.by_slug(params[:slug])
-  if page.nil?
-    redirect '/user/signin' unless session[:uid]
-    page = Page.new({:slug => params[:slug]}).save
-  else
-    user = User.by_id(session[:uid])
-    if user.can_edit_page?(page)
-      blocks = page.load_blocks
-      headers "Content-Type" => "text/html"
-      erb :page, :locals => {:page => page.to_s, :blocks => blocks.to_s}
-    else
-      'cant edit'
-    end
-  end
 end
 
 post '/page/:pid/block' do
@@ -84,5 +88,24 @@ delete '/page/:pid/block/:bid' do
   block.delete
   'OK'
 end
+
+get '/page/*' do
+  slug = params[:splat].first.to_slug
+  page   = Page.by_slug(slug)
+  if page.nil?
+    redirect '/user/signin' unless session[:uid]
+    page = Page.new({:slug => slug}).save
+  else
+    user = User.by_id(session[:uid])
+    if user.can_edit_page?(page)
+      blocks = page.load_blocks
+      return_html
+      erb :page, :locals => {:page => page.to_s, :blocks => blocks.to_s}
+    else
+      'cant edit'
+    end
+  end
+end
+
 
 #end
